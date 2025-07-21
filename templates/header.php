d68b4760bdc0e55696fb682c99c517e090520be0
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-07-22 00:42:46 +0200

    Modification Docker+mise en place de la page d'accueil

---
+++ templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <title>SoliDev - Votre Solution Web</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none ms-5">
+                <img width="90" src="../assets/images/logo-png.png" alt="Logo SoliDev">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    <a class="nav-link" href="#">Forum</a>
+                    <a class="nav-link" href="#">Blog</a>
+                    <a class="nav-link" href="#">Contact</a>
+                </div>
+                <div class="ms-auto">
+                    <button type="button" class="btn btn-outline-primary me-3">Connexion</button>
+                    <button type="button" class="btn btn-primary">Deconnexion</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ templates/home.php
@@ -0,0 +1,50 @@
+<?php require_once "./templates/header.php" ?>
+<main class="container">
+    <section >
+        <div class="border rounded">
+        <h1>Bonjour et bienvenue sur SoliDev !</h1>
+            <p> La plateforme pensée par et pour les développeurs: <br> 
+                Partage d'astuces, entraide, actus tech et projet open source. <br>
+                Que vous soyez débutant ou expert, ici on code solidaire</p>
+        </div>
+    </section>
+    <section>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <div class="row text-center">
+            <div class="col-md-4 my-2 d-flex">
+                <div class="card">
+                        <img src=" uploads/books/1-1984.jpg" class="card-img-top" alt="Desc">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 my-2 d-flex">
+                <div class="card">
+                    <img src=" uploads/books/2-histoires-courtes.jpg" class="card-img-top" alt="Desc">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 my-2 d-flex">
+                <div class="card">
+                    <img src=" uploads/books/3-zai-zai-zai-zai.jpg" class="card-img-top" alt="Desc">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php require_once "./templates/header.php" ?>
