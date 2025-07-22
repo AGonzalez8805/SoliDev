c8678f193b18df7c15db6519e44e471ed6f859c3
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-07-22 16:37:08 +0200

    Création dossier views

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+<footer> 
+    <ul class="nav justify-content-center border-bottom border-black pb-3 mb-3"> 
+        <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Mentions Légales</a>
+        </li> 
+        <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">A propos</a>
+        </li> 
+        <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">FAQs</a>
+        </li> 
+    </ul> 
+    <p class="text-center text-body-secondary">© 2025 SoliDev. Tous droits réservés.</p> 
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ views/page/about.php
@@ -0,0 +1,7 @@
+<?php 
+require_once _ROOTPATH_ . '/views/header.php';
+?>
+<h1>À propos</h1>
+<?php 
+require_once _ROOTPATH_ . '/views/footer.php';
+?>
