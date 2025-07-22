8c87489e6776a7f5faa2e7c9022b66c6dcee62c4
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-07-22 16:36:31 +0200

    Première étape de la création route+ test page

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -9,31 +9,8 @@ class Controller{
             switch ($_GET['controller']) {
                 case 'page':
                     //charger controleur page
-                    var_dump('On charge PageController');
-                    break;
-                case 'projet':
-                    //charger controleur projet
-                    var_dump('On charge ProjetController');
-                    break;
-                case 'forum':
-                    //charger controleur projet
-                    var_dump('On charge ForumController');
-                    break;
-                case 'Auth':
-                    //charger controleur projet
-                    var_dump('On charge AuthController');
-                    break;
-                case 'blog':
-                    //charger controleur projet
-                    var_dump('On charge BlogController');
-                    break;
-                case 'snippet':
-                    //charger controleur projet
-                    var_dump('On charge SnippetController');
-                    break;
-                case 'user':
-                    //charger controleur projet
-                    var_dump('On charge UserController');
+                    $pageController = new PageController();
+                    $pageController->route();
                     break;
                 
                 default:
@@ -42,6 +19,17 @@ class Controller{
             }
         }else {
             //Charger la page d'accueil
+        }
+    }
+
+    protected function render(string $path, array $params = []):void
+    {
+        $filePath = _ROOTPATH_. '/views/'.$path.'.php';
+
+        if(!file_exists($filePath)){
+            // Générer erreur
+        }else {
+            require_once $filePath;
         }
     }
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,3 +1,43 @@
 <?php
 
 namespace App\Controller;
+
+class PageController extends Controller
+{
+     public function route(): void //Ne retourne rien
+    {
+        if (isset($_GET['action'])){
+            switch ($_GET['action']) {
+                case 'home':
+                    // appeler la méthode home()
+                    $this->home();
+                    break;
+                case 'about':
+                    // appeler la méthode about();
+                    $this->about();
+                
+                default:
+                    //Erreur
+                    break;
+            }
+        }else {
+            //Charger la page d'accueil
+        }
+    }
+
+    protected function about()
+    {
+        // On pourrait récupérer les données
+        // en faisant appel au modèle
+
+        $this->render('page/about');
+    }
+
+        protected function home()
+    {
+        // On pourrait récupérer les données
+        // en faisant appel au modèle
+
+        $this->render('page/home');
+    }
+}
