c5706a6e2509ed78ba99e83bb81a9cd24cd67074
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-07-22 17:50:42 +0200

    création de la page default

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,21 +5,29 @@ namespace App\Controller;
 class Controller{
     public function route(): void //Ne retourne rien
     {
-        if (isset($_GET['controller'])){
-            switch ($_GET['controller']) {
-                case 'page':
-                    //charger controleur page
-                    $pageController = new PageController();
-                    $pageController->route();
-                    break;
-                
-                default:
-                    # code...
-                    break;
+        try {
+            if (isset($_GET['controller'])){
+                switch ($_GET['controller']) {
+                    case 'page':
+                        //charger controleur page
+                        $pageController = new PageController();
+                        $pageController->route();
+                        break;
+                    
+                    default:
+                        throw new \Exception("Le controleur n'existe pas");
+                        break;
+                }
+            }else {
+                //Charger la page d'accueil
             }
-        }else {
-            //Charger la page d'accueil
+        } catch (\Exception $e) {
+            $this->render('errors/default' , [
+                'error' => $e->getMessage()
+            ]);
+            
         }
+
     }
 
     protected function render(string $path, array $params = []):void
---
+++ views/errors/default.php
@@ -0,0 +1,9 @@
+<?php require_once _ROOTPATH_ . '/views/header.php'; ?>
+
+<?php if($error){?>
+<div class="alert alert-danger">
+    <?=$error;?>
+</div>
+<?php }?>
+
+<?php require_once _ROOTPATH_ . '/views/footer.php'; ?>
